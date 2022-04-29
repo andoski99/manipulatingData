5f2c45c0b36fedf3c35533ebbd166331f6fecbd0
andoski99 <goodsportdevelopment@icloud.com>
2022-04-30 06:21:48 +1000

    EndOfSession

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 //TODO: Extract an element from the array.
+    //const manipulatedData = companiesData[0];
+    // const manipulatedData = companiesData[0].fieldData.CompanyName;
+    // const manipulatedData = companiesData[0].fieldData?.CompanyName || "No Records";
+
 //TODO: Add elements to beginning and end of array.
+// const manipulatedData = companiesData.push(extraRecord);
+// const manipulatedData = companiesData.unshift(extraRecord); puts at the beginning of the array
+
 //TODO: Remove Elements from end of array.
 //TODO: Create a new array from old data.
 //TODO: Filter for a certain state.
@@ -19,5 +26,6 @@ before.innerHTML = JSON.stringify(companiesData, null, 2);
 //TODO: Create a new array with just elements of a certain state.
 //TODO: Create buttons for each record.
 //functions are here
-const manipulatedData = [companiesData[0]];
+const manipulatedData = companiesData.shift();
+const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
